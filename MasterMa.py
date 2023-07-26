62b073984bc24e6700266f32379b39c04c513e9b
kzhang19 <65601837+zhangkai0425@users.noreply.github.com>
2023-07-26 13:33:34 +0800

    Add files via upload

---
+++ MasterMa.py
@@ -0,0 +1,42 @@
+from transformers import AutoTokenizer, AutoModel
+import PySimpleGUI as sg
+
+# text1 = sg.popup_get_text('马老师说：',title='松果弹抖闪电鞭')
+# print(text1)
+tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm2-6b", trust_remote_code=True)
+model = AutoModel.from_pretrained("THUDM/chatglm2-6b", trust_remote_code=True).half().cuda()
+
+def MasterMa(model,text,history):
+    model = model.eval()
+    response, history = model.chat(tokenizer, "假如你是马保国老师，我是英国大理石，请问：" + text, history=history)
+    return response, history
+
+def ChatBot(model):
+    layout = [[(sg.Text('马老师说：武林要以和为贵，不要搞窝里斗！', size=[40, 1]))],
+              [sg.Output(size=(80, 20))],
+              [sg.Multiline(size=(70, 5), enter_submits=True),
+               sg.Button('偷袭一下', button_color=(sg.YELLOWS[0], sg.BLUES[0])),
+               [(sg.Text('英国大理石：', size=[40, 1]))],
+               sg.Button('退出聊天', button_color=(sg.YELLOWS[0], sg.BLUES[0]))]]
+
+    window = sg.Window('马老师和英国大理石', layout, default_element_size=(30, 2))
+    history = []
+    while True:
+        event, value = window.read()
+        if event == '偷袭一下':
+            text = value[1]
+            response, history = MasterMa(model=model,text=text,history=history)
+            print("英国大理石说：" + text)
+            print("马老师教导说：" + response)
+        else:
+            break
+    window.close()
+ChatBot(model=model)
+
+# tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm2-6b", trust_remote_code=True)
+# model = AutoModel.from_pretrained("THUDM/chatglm2-6b", trust_remote_code=True).half().cuda()
+# model = model.eval()
+# response, history = model.chat(tokenizer, "你好", history=[])
+# print(response)
+# response, history = model.chat(tokenizer, "晚上睡不着应该怎么办", history=history)
+# print(response)
